b66d7514ddaaff984922b25a01835837f6526d07
Eivind Stray <eivindhstray@gmail.com>
2020-10-31 21:49:08 +0100

    working model semi-copied from the internet

--- Warmup-CatsN'Dogs/models.py
+++ Warmup-CatsN'Dogs/models.py
@@ -1,5 +1,100 @@
 import os
 from tensorflow.keras import layers
 from tensorflow.keras import Model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
+import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+from tensorflow.keras.optimizers import RMSprop
+
+                    
+import matplotlib.image  as mpimg
+import matplotlib.pyplot as plt
+
+
+train_cat_dir = "archive/dataset/training_set/cats"
+train_dog_dir = "archive/dataset/training_set/dogs"
+validation_cat_dir = "archive/dataset/test_set/cats"
+validation_dog_dir = "archive/dataset/test_set/dogs"
+
+
+train_datagen = ImageDataGenerator(rescale = 1./255.,
+                                   rotation_range = 40,
+                                   width_shift_range = 0.2,
+                                   height_shift_range = 0.2,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+
+test_datagen = ImageDataGenerator( rescale = 1.0/255. )
+
+train_generator = train_datagen.flow_from_directory("archive/dataset/training_set",
+                                                    batch_size = 40,
+                                                    class_mode = 'binary', 
+                                                    target_size = (150, 150))     
+
+validation_generator =  test_datagen.flow_from_directory( "archive/dataset/test_set",
+                                                          batch_size  = 40,
+                                                          class_mode  = 'binary', 
+                                                          target_size = (150, 150))
+
+
+                                                    
+
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+
+
+model.compile(optimizer=RMSprop(lr=0.001),
+              loss='binary_crossentropy',
+              metrics = ['accuracy'])
+
+
+history = model.fit(train_generator,
+                              validation_data=validation_generator,
+                              steps_per_epoch=10,
+                              epochs=10,
+                              validation_steps=50,
+                              verbose=2)
+
+
+
+
+acc=history.history['accuracy']
+val_acc=history.history['val_accuracy']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(len(acc)) 
+
+
+plt.plot(epochs, acc, 'r', "Training Accuracy")
+plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
+plt.title('Training and validation accuracy')
+plt.figure()
+
+
+plt.plot(epochs, loss, 'r', "Training Loss")
+plt.plot(epochs, val_loss, 'b', "Validation Loss")
+
+
+plt.title('Training and validation loss')
